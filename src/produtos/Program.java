b198222dd42af24062e041a1c116d5c5d0f14086
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-12-18 11:54:31 -0300

    Finalizando algorítmo para encontrar o carro mais barato.

--- src/produtos/Program.java
+++ src/produtos/Program.java
@@ -13,20 +13,26 @@ public class Program {
 				new Produto("Jipe", 46000),
 				new Produto("Brasilia", 16000),
 				new Produto("Smart", 460000),
-				new Produto("Fusca", 17000)
+				new Produto("Fusca", 17000),
+				null,
+				null,
+				null,
+				null,
+				null
 		};
 		
 		
-		int maisBarato = buscaMenor(produtos);
+		int maisBarato = buscaMenor(produtos, 4);
 		System.out.println(maisBarato);
 		System.out.print("O carro " + produtos[maisBarato].getNome() + "é o mais barato e custa " + produtos[maisBarato].getPreco());
 		
 
 	}
 	
-	public static int buscaMenor(Produto[] produtos) {
+	public static int buscaMenor(Produto[] produtos, int termino) {
 		int maisBarato = 0;
-		for(int atual = 0; atual <= 4; atual++) {
+//		int termino = produtos.length - 1;
+		for(int atual = 0; atual <= termino; atual++) {
 			if(produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
 				maisBarato = atual;
 			}
